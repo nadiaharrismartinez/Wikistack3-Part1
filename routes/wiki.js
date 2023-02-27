a8ab081910d960d1ba9069350a8ed17e3920a591
nadiakhristean <nadia.khristean@gmail.com>
2023-02-27 15:14:11 -0500

    Added get and post routes for homepage and slug pages

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,16 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { Page } = require('../models');
-const { addPage } = require('../views');
+const { main, addPage, wikiPage } = require('../views');
 
-router.get('/', (req, res, next) => {
-  res.send('got to GET /wiki/');
+router.get('/', async (req, res, next) => {
+  try {
+    const pages = await Page.findAll();
+    res.send(main(pages));
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post('/', async (req, res, next) => {
   try {
     const page = await Page.create(req.body);
-    res.redirect('/');
+    res.redirect(`/wiki/${page.slug}`);
   } catch (error) {
     next(error);
   }
@@ -18,6 +23,19 @@ router.post('/', async (req, res, next) => {
 
 router.get('/add', (req, res, next) => {
   res.send(addPage());
+});
+
+router.get('/:slug', async (req, res, next) => {
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug,
+      },
+    });
+    res.send(wikiPage(page));
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
